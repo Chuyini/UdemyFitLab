52b8162dccc83cd784a86a610a7476dc097dc31a
Jesus Manuel <jmlr231201@gmail.com>
2024-11-07 16:21:11 -0600

    Booksy completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,17 @@ class Booksy extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       //Hay restricciones
+      padding: EdgeInsets.symmetric(
+          horizontal: 40), //<-- solo container recibe este parametro
 
       child: Center(
         child: Column(
           //Column recibe a fuerzas un childre?
+
           children: [
+            Padding(
+              padding: EdgeInsets.only(top: 100.0),
+            ),
             Container(
               decoration: BoxDecoration(
                 boxShadow: [BoxShadow(blurRadius: 8)],
@@ -24,17 +30,64 @@ class Booksy extends StatelessWidget {
                 //a recursos del proyecto
               ),
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 30.0),
+            ),
             //Los textos requieren direccionamiento
             Text(
-              "Titulo",
+              "The Echo Enigma",
               textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
             ),
-            Text("Author", textDirection: TextDirection.ltr),
-            Text("", textDirection: TextDirection.ltr)
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+
+            Text(
+              "Elena Ferrante",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 20,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+
+            Text(
+              "In a coastal Italian village, archaeologist Lucia Moretti discovers an ancient manuscript that could change history. Pursued by a "
+              "secret society, she must unravel the manuscript’s mysteries and uncover her family’s hidden past before it’s too late.",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 20,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+
+            Directionality(
+              //<-- de ley se ocupa para que envuelva al elevatedButton  y darle direccion si no nos marca error
+              textDirection: TextDirection.ltr,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Agregar a la libreria "),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                ),
+              ),
+            )
+            //<-- al onPress se le teiene que pasar una funicion vacia, si es null se desactiva
+            //Elevated button ademas no tiene una direccion hay que darsela en el child en ese caso un text con ese constructor
           ],
         ),
       ),
-      color: Colors.blueGrey,
+      color: Colors.white70,
     );
     // TODO: implement build
     throw UnimplementedError();
